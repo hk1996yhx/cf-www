6adc4b00c7f2107a16c2862fb5071b7d76354d3a
hk1996yhx <50992362+hk1996yhx@users.noreply.github.com>
2025-08-01 20:32:49 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -50,11 +50,8 @@ const HTML_CONTENT = `
         document.getElementById('contentFrame').src = url;
     }
 </script>
-<script src="/front/dist/index.min.js" data-base-url="cf-analytics.377020945.workers.dev"></script>
-<div>
-本页访问人次:<span id="page_pv"></span>
-
-本页访问人数:<span id="page_uv"></span></div>
+<script defer src="//webviso.yestool.org/js/index.min.js"></script>
+本页访问人次:<span id="page_pv"></span>本页访问人数:<span id="page_uv"></span>
     <!-- 看板娘 -->
     <script src="https://unpkg.com/oh-my-live2d@latest"></script>
     <script>
